dd4d2e27b60c666f556248d6b43350430d98fc90
Emmanuel Dupin <seedfloyd@gmail.com>
2010-09-10 20:08:30 +0200

    French translation added

---
+++ lang/fr/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * French language file
+ *
+ * @author Emmanuel Dupin <seedfloyd@gmail.com>
+ */
+ 
+$lang['name']  = 'Configuration des redirections';
+$lang['saved'] = 'Données enregistrées.';
+
+$lang['redirected'] = 'Redirigé depuis <b>%s</b>.';
---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * French language file
+ *
+ * @author Emmanuel Dupin <seedfloyd@gmail.com>
+ */
+
+$lang['showmsg'] = 'Afficher un message de redirection aux utilisateurs ?';
