02912255d5f6b03267a35c88b59e302684e756d2
Myeongjin <aranet100@gmail.com>
2013-08-13 14:50:06 +0200

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -1,3 +1,8 @@
 <?php
 
-$lang['showmsg'] = '사용자에서 넘어왔슴을 알릴까요?';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['showmsg']               = '사용자에게 넘겨주기에 대한 메시지를 보여줄까요?';
