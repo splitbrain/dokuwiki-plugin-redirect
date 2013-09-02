1355004882e81bd49d73447d1e05efe1202a1264
Aleksandr Selivanov <alexgearbox@gmail.com>
2013-09-02 21:20:10 +0200

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -1,6 +1,9 @@
 <?php
 
-$lang['name']  = 'Переадресация страниц';
-$lang['saved'] = 'Данные сохранены.';
-
-$lang['redirected'] = 'Вы перенаправлены с <b>%s</b>.';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['name']                  = 'Переадресация страниц';
+$lang['saved']                 = 'Данные сохранены.';
+$lang['redirected']            = 'Вы перенаправлены с <b>%s</b>.';
