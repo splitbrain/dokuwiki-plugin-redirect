4b231beec0ce746861e8ac0e94d36fbf74c35a2c
Gerrit Uitslag <klapinklapin@gmail.com>
2013-11-12 20:47:04 +0100

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ */
+$lang['name']                  = 'Pagina-doorstuur instellingen';
+$lang['saved']                 = 'Gegevens opgeslagen.';
+$lang['redirected']            = 'Je bent doorgestuurd van <b>%s</b>.';
