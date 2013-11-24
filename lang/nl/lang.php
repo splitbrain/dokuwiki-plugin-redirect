b93089aed1303ad1b29a0a259ee52aa839c0fc38
Mark C. Prins <mprins@users.sf.net>
2013-11-24 18:02:37 +0100

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Mark C. Prins <mprins@users.sf.net>
+ */
+$lang['name']                  = 'Pagina doorsturen instellingen';
+$lang['saved']                 = 'Gegevens opgeslagen.';
+$lang['redirected']            = 'u werd doorgestuurd van <b>%s</b>.';
---
+++ lang/nl/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Mark C. Prins <mprins@users.sf.net>
+ */
+$lang['showmsg']               = 'Toon een bericht aan de gebruiker over het doorsturen?';
