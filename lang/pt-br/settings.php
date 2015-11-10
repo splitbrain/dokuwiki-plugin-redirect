e08883c69e1f6f90e2416b5bfd35c0fc027480f6
Flaudísio Tolentino <flaudisio@gmail.com>
2015-11-10 18:15:10 +0100

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Flaudísio Tolentino <flaudisio@gmail.com>
+ */
+$lang['showmsg']               = 'Mostrar uma mensagem sobre o redirecionamento aos usuários?';
