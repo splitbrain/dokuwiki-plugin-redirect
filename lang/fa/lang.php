fe16469ce461d05ecd11b564665086b6307f6b04
Sam01 <m.sajad079@gmail.com>
2016-08-24 15:20:40 +0200

    translation update

---
+++ lang/fa/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['name']                  = 'برپایی تغییرمسیر صفحه';
+$lang['saved']                 = 'تاریخ ذخیره شد.';
+$lang['redirected']            = 'شما تغییرمسیر یافته‌اید از <b>%s</b>.';
