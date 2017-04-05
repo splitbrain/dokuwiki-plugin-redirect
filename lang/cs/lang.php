53a4b94bb977cd955de981f0b3acdd3e5ee7fb3a
Pavel Krupička <pajdacz@gmail.com>
2017-04-05 14:30:12 +0200

    translation update

---
+++ lang/cs/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Pavel Krupička <pajdacz@gmail.com>
+ */
+$lang['name']                  = 'Nastavení přesměrování stránek';
+$lang['saved']                 = 'Data uložena.';
+$lang['redirected']            = 'Byl jste přesměrován z <b>%s</b>.';
