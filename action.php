4472a23c20e7f3b78e955d19ba2687173e58cf75
Andreas Gohr <andi@splitbrain.org>
2017-11-05 10:15:18 +0100

    file cleanup

--- action.php
+++ action.php
@@ -9,52 +9,57 @@
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'action.php');
 
 class action_plugin_redirect extends DokuWiki_Action_Plugin {
 
     /**
      * register the eventhandlers
+     * 
+     * @param Doku_Event_Handler $controller
      */
-    function register(Doku_Event_Handler $controller){
-        $controller->register_hook('DOKUWIKI_STARTED',
-                                   'AFTER',
-                                   $this,
-                                   'handle_start',
-                                   array());
+    function register(Doku_Event_Handler $controller) {
+        $controller->register_hook(
+            'DOKUWIKI_STARTED',
+            'AFTER',
+            $this,
+            'handle_start',
+            array()
+        );
     }
 
     /**
      * handle event
+     *
+     * @param Doku_Event $event
+     * @param array $param
      */
-    function handle_start(&$event, $param){
+    function handle_start(Doku_Event $event, $param) {
         global $ID;
         global $ACT;
+        global $INPUT;
 
         if($ACT != 'show') return;
 
-        $redirects = confToHash(dirname(__FILE__).'/redirect.conf');
-        
-        if (isset($_GET['redirect']) && $_GET['redirect'] == 'no') {
-			// return if redirection is temporarily by url paramter disabled,
-				return;
-        }else{
-            if($redirects[$ID]){
-                if(preg_match('/^https?:\/\//',$redirects[$ID])){
+        $redirects = confToHash(dirname(__FILE__) . '/redirect.conf');
+
+        if($INPUT->get->str('redirect') == 'no') {
+            // return if redirection is temporarily disabled by url parameter
+            return;
+        } else {
+            if($redirects[$ID]) {
+                if(preg_match('/^https?:\/\//', $redirects[$ID])) {
                     send_redirect($redirects[$ID]);
-                }else{
-                    if($this->getConf('showmsg')){
-                        msg(sprintf($this->getLang('redirected'),hsc($ID)));
+                } else {
+                    if($this->getConf('showmsg')) {
+                        msg(sprintf($this->getLang('redirected'), hsc($ID)));
                     }
                     $link = explode('#', $redirects[$ID], 2);
-                    send_redirect(wl($link[0] ,'',true) . '#' . rawurlencode($link[1]));
+                    send_redirect(wl($link[0], '', true) . '#' . rawurlencode($link[1]));
                 }
                 exit;
             }
         }
     }
-
 
 }
 
